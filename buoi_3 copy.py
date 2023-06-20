6deb0300df7ecea17a4f26add94829cd44a8402c
Xuanphudeptrai <136835885+Xuanphudeptrai@users.noreply.github.com>
2023-06-20 14:39:31 +0700

    Add files via upload

---
+++ buoi_3 copy.py
@@ -0,0 +1,25 @@
+vn = 17600
+cana = 1 
+print("1 do la cua canada khi doi sang vn la: " + str(vn/cana) )
+vnd= 23200
+us = 1
+
+print("1 usd doi sang vn  la:"+ str(vnd/us ))
+vnd = 16590
+yen = 1
+print(" 100 yen doi sang vnd la: " + str(vnd/yen))
+
+vnd = 28726
+rubi = 1
+print(" 1 rubi cua an do doi sang vn la: " + str(vnd/rubi))
+ 
+vnd= 1
+won = 54
+print("1 won bang vnd la "+ str(won/vnd))
+ 
+vnd = 2570736 
+euro = 1 
+print ("1 euro doi sang vn la " + str(vnd/euro) )
+rupeeNepal = 1
+vnd  = 17909500 
+print("1 rupee nepal doi sang vnd la " + str(vnd/rupeeNepal))
