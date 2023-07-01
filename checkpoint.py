cf2a1970181dcf100dbab10136fe6508ad3feaee
Xuanphudeptrai <136835885+Xuanphudeptrai@users.noreply.github.com>
2023-07-01 19:57:21 +0700

    Add files via upload

---
+++ checkpoint.py
@@ -0,0 +1,11 @@
+a = int (float(input("hay nhap  so kwh ma bạn tiêu thụ hàng tháng ")))
+if (  200 >= a):
+    print(" đơn giá của bạn là 1768 đồng/ kwh")
+elif (  300 >= a ):
+       print(" đơn giá của bạn là 2074 đồng/ kwh")
+elif ( 400>=a ):
+       print(" đơn giá của bạn là 2612 đồng/ kwh")
+elif ( a>= 400):
+       print(" đơn giá của bạn là 3015 đồng/ kwh")    
+if(  50>= a ):
+        print( " đơn giá của bạn là 1728")
